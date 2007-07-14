5c981d6eab541f8e8504fd0c1ec3dc1ac55e5060
jmathess <jmathess@2c38486a-e032-0410-af45-512d92457bae>
2007-07-14 13:11:52 +0000

    Forgotten files for NEC SX cpu and coff added.

---
+++ trunk/sx_binutils/bfd/coff-sx.c
@@ -0,0 +1,12 @@
+/* BFD back-end for SX coff file
+   Copyright 2007
+   Original version Matthias Hess
+*/
+
+#include "sysdep.h"
+#include "bfd.h"
+#include "libbfd.h"
+
+#include "coff/sx.h"
+#include "coff/internal.h"
+
